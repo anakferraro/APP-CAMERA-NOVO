5d461d8d78b5e3bf94a12daa34ad205eb2ecb096
Ana Karolina Ferraro <ferraroanakarolina@gmail.com>
2025-09-01 14:14:06 -0300

    Delete app directory

--- app/_layout.js
+++
@@ -1,9 +0,0 @@
-// Layout do Expo Router
-// Define stack navigator do app
-
-import { Stack } from "expo-router";
-
-export default function Layout() {
-  // Stack com header escondido
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
